14d4e9d6dd1c83295ee057286115a9638c1478ee
bhavikmaradiya <68755759+bhavikmaradiya@users.noreply.github.com>
2021-04-28 16:48:17 +0530

    remove user, update message, make admin

--- app.py
+++ app.py
@@ -75,18 +75,26 @@ def createRandomCode():
 def restrictUserFromGroup():
     gId = request.form["gId"] if request.form.get("gId") else None
     userId = request.form["userId"] if request.form.get("userId") else None
+    currentUserId = request.form["loggedId"] if request.form.get("loggedId") else None
     restrict = request.form["restrict"] if request.form.get("restrict") else None
-    if gId and userId and restrict:
-        if db.groupList.find_one({"gId": gId, "users.id": userId}):
-            db.groupList.update({
-                'gId': gId,
-                'users.id': userId
-            }, {
-                "$set": {"users.$.restrict": True if restrict.lower() == "true" else False}
-            })
-            return json.dumps({"status": 1, "message": "User restriction changed"})
+    if gId and userId and restrict and currentUserId:
+        group = db.groupList.find_one({'gId': gId})
+        if group:
+            if group['adminId'] == currentUserId:
+                if db.groupList.find_one({"gId": gId, "users.id": userId}):
+                    db.groupList.update({
+                        'gId': gId,
+                        'users.id': userId
+                    }, {
+                        "$set": {"users.$.restrict": True if restrict.lower() == "true" else False}
+                    })
+                    return json.dumps({"status": 1, "message": "User restriction changed"})
+                else:
+                    return json.dumps({"status": 0, "message": "Something went wrong"})
+            else:
+                return json.dumps({"status": 0, "message": "You can't change admin"})
         else:
-            return json.dumps({"status": 0, "message": "Something went wrong"})
+            return json.dumps({"status": 0, "message": "No group exist"})
     else:
         return json.dumps({"status": 0, "message": "All fields are required"})
 
@@ -94,33 +102,101 @@ def restrictUserFromGroup():
 @app.route('/addUserToGroup', methods=['POST'])
 def addUserToGroup():
     gId = request.form["gId"] if request.form.get("gId") else None
+    loggedId = request.form["loggedId"] if request.form.get("loggedId") else None
     shareId = request.form["shareId"] if request.form.get("shareId") else None
-    if gId and shareId:
+
+    if gId and shareId and loggedId:
         dbUser = db.users.find_one({"shareId": shareId})
         dbGroup = db.groupList.find_one({"gId": gId})
         if dbUser and dbGroup:
-            if db.groupList.find_one({"gId": gId, "users.id": dbUser['userId']}) is None:
-                db.groupList.update({'gId': gId}, {
-                    "$addToSet": {
-                        "users": {
-                            "id": dbUser['userId'],
-                            "restrict": False
+            if dbGroup['adminId'] == loggedId:
+                if db.groupList.find_one({"gId": gId, "users.id": dbUser['userId']}) is None:
+                    db.groupList.update({'gId': gId}, {
+                        "$addToSet": {
+                            "users": {
+                                "id": dbUser['userId'],
+                                "restrict": False
+                            }
                         }
-                    }
-                })
-            if db.users.find_one({"userId": dbUser['userId'], "groups.gId": gId}) is None:
-                db.users.update({'userId': dbUser['userId']}, {
-                    "$addToSet": {
-                        "groups": {
-                            "gId": gId
+                    })
+                if db.users.find_one({"userId": dbUser['userId'], "groups.gId": gId}) is None:
+                    db.users.update({'userId': dbUser['userId']}, {
+                        "$addToSet": {
+                            "groups": {
+                                "gId": gId
+                            }
                         }
-                    }
-                })
-            return json.dumps({"status": 1, "message": "User added successfully"})
+                    })
+                return json.dumps({"status": 1, "message": "User added successfully"})
+            else:
+                return json.dumps({"status": 0, "message": "You can't add participate"})
         else:
             return json.dumps({"status": 0, "message": "Something went wrong"})
     else:
         return json.dumps({"status": 0, "message": "All fields are required"})
+
+
+@app.route('/updateMessage', methods=['POST'])
+def updateMessage():
+    gId = request.form["gId"] if request.form.get("gId") else None
+    userId = request.form["loggedId"] if request.form.get("loggedId") else None
+    message = request.form["message"] if request.form.get("message") else ""
+
+    if gId and userId:
+        group = db.groupList.find_one({"gId": gId, "users.id": userId})
+        if group:
+            if \
+                    db.groupList.find_one({"gId": gId, "users.id": userId}, {"users": {"$elemMatch": {"id": userId}}})[
+                        'users'][
+                        0]['restrict'] is not True:
+                db.groupList.update({'gId': gId}, {'$set': {'message': message}})
+                return json.dumps({"status": 1, "message": "message updated successfully"})
+            else:
+                return json.dumps({"status": 0, "message": "You can't edit this note"})
+        else:
+            return json.dumps({"status": 0, "message": "Something went wrong"})
+    else:
+        return json.dumps({"status": 0, "message": "All field are required"})
+
+
+@app.route('/removeMember', methods=['POST'])
+def removeMember():
+    gId = request.form["gId"] if request.form.get("gId") else None
+    currentUserId = request.form["loggedId"] if request.form.get("loggedId") else None
+    userId = request.form["userId"] if request.form.get("userId") else None
+
+    if gId and currentUserId and userId:
+        group = db.groupList.find_one({'gId': gId, "users.id": userId})
+        if group:
+            if group['adminId'] == currentUserId:
+                db.groupList.update({'gId': gId}, {'$pull': {'users': {'id': userId}}})
+                return json.dumps({"status": 1, "message": "User removed successfully"})
+            else:
+                return json.dumps({"status": 0, "message": "You can't remove participate"})
+        else:
+            return json.dumps({"status": 0, "message": "Something went wrong"})
+    else:
+        return json.dumps({"status": 0, "message": "All field are required"})
+
+
+@app.route('/makeAdmin', methods=['POST'])
+def makeAdmin():
+    gId = request.form["gId"] if request.form.get("gId") else None
+    currentUserId = request.form["loggedId"] if request.form.get("loggedId") else None
+    userId = request.form["userId"] if request.form.get("userId") else None
+
+    if gId and currentUserId and userId:
+        group = db.groupList.find_one({'gId': gId})
+        if group:
+            if group['adminId'] == currentUserId:
+                db.groupList.update({'gId': gId}, {'$set': {'adminId': userId}})
+                return json.dumps({"status": 1, "message": "Admin changed successfully"})
+            else:
+                return json.dumps({"status": 0, "message": "You can't change admin"})
+        else:
+            return json.dumps({"status": 0, "message": "No group exist"})
+    else:
+        return json.dumps({"status": 0, "message": "All field are required"})
 
 
 @app.route('/createNote', methods=['POST'])
@@ -175,21 +251,16 @@ def updateUser():
     userName = request.form["userName"] if request.form.get("userName") else None
     profileUrl = request.form["profileUrl"] if request.form.get("profileUrl") else None
     groupLimit = request.form["groupLimit"] if request.form.get("groupLimit") else None
-    user = {}
     if userId is None:
         return json.dumps({"status": 0, "message": "User id is required"})
-    dbUser = db.users.find_one({"userId": userId})
-    if dbUser is not None:
-        user["userName"] = userName if userName else dbUser["userName"]
-        user["emailId"] = dbUser["emailId"]
-        user["userId"] = dbUser["userId"]
-        user["shareId"] = dbUser["shareId"]
-        user["groupLimit"] = groupLimit if groupLimit else dbUser["groupLimit"]
-        if dbUser.get("groups"):
-            user["groups"] = dbUser["groups"]
-        if profileUrl is not None or dbUser.get("profileUrl"):
-            user["profileUrl"] = profileUrl if profileUrl else dbUser["profileUrl"]
-        db.users.update({'userId': userId}, user)
+    if db.users.find_one({"userId": userId}) is not None:
+        if profileUrl is not None:
+            db.users.update({'userId': userId}, {"$set": {"profileUrl": profileUrl}})
+        if userName is not None:
+            db.users.update({'userId': userId}, {"$set": {"userName": userName}})
+        if groupLimit is not None:
+            db.users.update({'userId': userId}, {"$set": {"groupLimit": int(groupLimit)}})
+
         return json.dumps({"status": 1, "message": "User updated successfully"})
     else:
         return json.dumps({"status": 0, "message": "No user exist"})
@@ -208,7 +279,7 @@ def insertNewUser():
         return json.dumps({"status": 0, "message": "User id is required"})
     if emailId is None:
         return json.dumps({"status": 0, "message": "email is required"})
-    user = {"profileUrl": profileUrl if profileUrl else "", "groupLimit": groupLimit if groupLimit else 5,
+    user = {"profileUrl": profileUrl if profileUrl else "", "groupLimit": int(groupLimit) if groupLimit else 5,
             "userId": userId, "emailId": emailId, "userName": userName,
             "shareId": userId[0: 3].upper() + createRandomCode().upper() + userId[1:3].upper()}
 
